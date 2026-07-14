30e9263e926d8fc54186c9b95aa6d307e7242ccd
Janhavi <janhavi.hariom2026@gmail.com>
2026-07-14 06:21:03 +0530

    Add files via upload

---
+++ Javascript/function.js
@@ -0,0 +1,190 @@
+/*function abc(){
+    console.log("hello")
+}
+abc()*/
+
+/*function greet(str){
+    console.log("Good evening.....",str)
+}
+greet("janhavi")
+greet(20)*/
+
+/*function abc(a,b){
+    console.log(a+b)
+}
+abc(10,20)
+mul(20,30)
+
+function mul(a,b){
+    console.log(a*b)
+}*/
+
+/*function party(user,age){
+    console.log("Good evening.....",user)
+    if(age>=18){
+        console.log("You are welcome to the party")
+    }else{
+        console.log("you are not allowed ")
+    }
+}
+party('janhavi', 12)
+party("laxmi",23)*/
+
+
+
+
+
+
+
+// Types of functions :
+//1. declaration function - 
+/*function a(){
+    console.log("heyyyy")
+}
+a()*/
+
+
+//2. function expression :
+/*let b= function(){
+    console.log("hi")
+}
+b()
+
+
+
+//3. fat-arrow function 
+var a= ()=>{
+    console.log("elu elu")
+}
+a()*/
+
+
+
+
+
+
+
+// IIFE - Immediately Invoked Function Expression 
+/*(function(){
+    console.log("This is IIFE ")
+}) ()*/
+/*(function(){
+    var a=10
+    console.log(a)
+}) ()
+console.log(a)*/
+
+
+
+
+
+// RETURN 
+/*function abc(){
+    console.log("hiiii")
+    return 200
+}
+var a=abc()
+console.log(abc())*/
+
+
+
+/*function abc(a,b){
+    return a+b
+}
+console.log(abc(20,20))*/
+
+
+
+/*function gen(){
+    if(gen=='F'){
+        /*return "Welcome to women party"*/
+         /*console.log("Welcome to female party")*/
+    /*}else{
+        /*return "Welcome to men party"*/
+        /*console.log("Welcome to man party"
+    }
+}
+console.log(gen("M"))*/
+
+/*var a=10
+function add(){
+ a++
+    return a
+}
+console.log(add())
+console.log(add())*/
+
+
+/*function sqrt(a){
+    return a*a
+}
+console.log(sqrt(5))*/
+
+/*function add(a,b){
+    return a+b
+}
+console.log(add(20,20))*/
+
+
+
+
+/*function sqrt(a){
+    return a*a
+}
+console.log(sqrt(5))
+console.log(sqrt(5)) this is pure 
+
+var a=10
+function b(){
+    a++
+    return a
+}
+console.log(b())
+console.log(b()) this is impure */
+
+
+/*let a=10
+function b(){
+    a++
+    return a
+}
+console.log(b())
+console.log(b())*/
+
+
+/*function greet(name='janhavi', age=20){
+    console.log("Welcome",name, age)
+}
+greet(undefined,90)*/
+
+
+
+
+// Callback function
+/*function hero(b){
+    console.log("I am the hero")
+}
+
+function main(a){
+    console.log("I am main")
+    a()
+}
+main(hero)*/
+
+
+
+
+
+var a= function(){
+    console.log("hello")
+}
+a()
+
+
+
+
+
+
+
+
+
---
+++ Javascript/script.js
@@ -0,0 +1,99 @@
+//console.log('hello js')
+//console.warn("this is warning")
+//console.error("this is error")
+//console.table(["hars", "jd"])
+
+// Variables:
+//var a=20;
+//var b=12;
+//console.log('the value of a is',a)
+//console.log(a+b)
+
+//var c="janhavi"
+//console.log(type)
+
+//var d
+//console.log(d)
+
+////var e=null;
+//console.log(e)
+
+//alert("are you 10+")
+//confirm("pakka na")
+
+//var a=Number(prompt("Enter a num-1 "))
+///var b=Number(prompt("Enter a num-1 "))
+//console.log(a+b)
+
+//var a=Number("10")
+//var b=30
+////console.log(a+b)
+
+//var a=10;
+//console.log(a++)
+
+//var a=10;
+//var b=20;
+//if(a>b){
+    //console.log("eeeee")
+//}else{
+    //console.log("clockitt")
+//}
+
+//var a=Number(prompt("Enter a num"))
+//if(a>85){
+    //console.log("you get scholoship")
+////}else{
+    //console.log("sorry no scholorship")
+//}
+
+//var phy=Number(prompt("Enter marks of phy"))
+//var chem=Number(prompt("Enter marks of chem"))
+//var bio=Number(prompt("Enter marks of bio"))
+//var avg=75
+//if(avg>=85){
+    //console.log("You are topper")
+//}else if(avg>=70){
+//console.log("Better")
+//} else{
+   // console.log("try again next time")
+//}
+
+
+
+
+
+
+
+//LOOPS 
+//while loop
+//var num= Number(prompt("enter a num"))
+//var a=0
+//while(a<=num){
+    //console.log(a)
+    //a++
+//}
+
+//var num= Number(prompt("Enter a  num"))
+//var a=0;
+//while(a<=num){
+//if(a%2==0){
+    //console.log(a)
+/////}
+
+//a++
+//} 
+
+// print table of any number 
+/*var num= Number(prompt("Enter any number"))
+var a= 0;
+while( a<=10){ // a<=num
+    console.log(num+'*' +a+'='+num*a)
+    
+    a++
+}*/
+
+
+
+
+
